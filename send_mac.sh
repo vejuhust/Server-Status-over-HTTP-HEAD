1c7d9c65a5482d0d79aee31ebbbe7f3164be7514
Wei Ye <vejuhust@gmail.com>
2015-08-11 23:49:25 +0800

    Add send_mac with name/message/ip/date/uptime/disk

---
+++ send_mac.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl http://service.yewei.me/ -I -A "$(echo "[name] $(hostname); [message] dev_mac; [ip] $(ifconfig | grep en1 -A 1 | tail -1); [date] $(date '+%Y-%m-%d %H:%M:%S %z %Z'); [uptime] $(uptime); [disk] $(df -h | grep / -m 1);" | sed -E "s/[[:space:]]+/ /gp")"
