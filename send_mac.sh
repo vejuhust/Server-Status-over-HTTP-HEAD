884ef0feb52b8afd2518d266eef51020f5ca81b8
Wei Ye <vejuhust@gmail.com>
2015-08-12 01:29:29 +0800

    Add '[ssohh] abcwtf;' for send_*.sh

--- send_mac.sh
+++ send_mac.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl http://service.yewei.me/ -I -A "$(echo "[name] $(hostname); [note] dev_mac; [network] $(/sbin/ifconfig | grep en1 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(top -l 1 | head -10 | grep PhysMem); " | sed -E "s/[[:space:]]+/ /gp")"
+curl http://service.yewei.me/ -I -A "$(echo "[ssohh] $SSOHH_TOKEN; [name] $(hostname); [note] dev_mac; [network] $(/sbin/ifconfig | grep en1 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(top -l 1 | head -10 | grep PhysMem); " | sed -E "s/[[:space:]]+/ /gp")"
--- send_pi.sh
+++ send_pi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl http://service.yewei.me/ -I -A "$(echo "[name] $(hostname); [note] dev_pi; [network] $(/sbin/ifconfig | grep eth0 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(cat /proc/meminfo | grep MemFree) / $(cat /proc/meminfo | grep MemTotal); [temperature] $(cat /sys/class/thermal/thermal_zone0/temp); " | sed -E "s/[[:space:]]+/ /gp")"
+curl http://service.yewei.me/ -I -A "[ssohh] $SSOHH_TOKEN; $(echo "[name] $(hostname); [note] dev_pi; [network] $(/sbin/ifconfig | grep eth0 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(cat /proc/meminfo | grep MemFree) / $(cat /proc/meminfo | grep MemTotal); [temperature] $(cat /sys/class/thermal/thermal_zone0/temp); " | sed -E "s/[[:space:]]+/ /gp")"
