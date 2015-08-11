74a88e58aaa976415ea37b28db5fd8f5d558fa35
Wei Ye <vejuhust@gmail.com>
2015-08-12 00:53:41 +0800

    Adjust send_mac.sh for sh in crontab

--- send_mac.sh
+++ send_mac.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-curl http://service.yewei.me/ -I -A "$(echo "[name] $(hostname); [note] dev_mac; [network] $(ifconfig | grep en1 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(top -l 1 | head -10 | grep PhysMem); " | sed -E "s/[[:space:]]+/ /gp")"
+curl http://service.yewei.me/ -I -A "$(echo "[name] $(hostname); [note] dev_mac; [network] $(/sbin/ifconfig | grep en1 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(top -l 1 | head -10 | grep PhysMem); " | sed -E "s/[[:space:]]+/ /gp")"
