0a328b7ac9a8b627e11543f3574aa4f0715680bc
Wei Ye <vejuhust@gmail.com>
2015-08-12 14:02:01 +0800

    Update tag, note & crontab interval in set_cron.sh

--- set_cron.sh
+++ set_cron.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SSOHH_TAG=abcwtf
+SSOHH_TAG=YOUR_PRIVATE_TAG_FOR_SSOHH
 
 if [ $# -ge 1 ];
 then
@@ -11,16 +11,16 @@ fi
 
 case $ARCH in
 pi)
-    MONITOR='"[ssohh] '$SSOHH_TAG'; [name] $(hostname); [note] cron_pi; [network] $(/sbin/ifconfig | grep eth0 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(cat /proc/meminfo | grep MemFree) / $(cat /proc/meminfo | grep MemTotal); [temperature] $(cat /sys/class/thermal/thermal_zone0/temp); "'
+    MONITOR='"[ssohh] '$SSOHH_TAG'; [name] $(hostname); [note] regular_5min_pi; [network] $(/sbin/ifconfig | grep eth0 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(cat /proc/meminfo | grep MemFree) / $(cat /proc/meminfo | grep MemTotal); [temperature] $(cat /sys/class/thermal/thermal_zone0/temp); "'
     ;;
 bbb)
-    MONITOR='"[ssohh] '$SSOHH_TAG'; [name] $(hostname); [note] cron_bbb; [network] $(/sbin/ifconfig | grep eth0 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(cat /proc/meminfo | grep MemFree) / $(cat /proc/meminfo | grep MemTotal); [temperature] $(cat /sys/class/hwmon/hwmon0/device/temp1_input); "'
+    MONITOR='"[ssohh] '$SSOHH_TAG'; [name] $(hostname); [note] regular_5min_bbb; [network] $(/sbin/ifconfig | grep eth0 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(cat /proc/meminfo | grep MemFree) / $(cat /proc/meminfo | grep MemTotal); [temperature] $(cat /sys/class/hwmon/hwmon0/device/temp1_input); "'
     ;;
 linux)
-    MONITOR='"[ssohh] '$SSOHH_TAG'; [name] $(hostname); [note] cron_linux; [network] $(/sbin/ifconfig | grep eth0 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(cat /proc/meminfo | grep MemFree) / $(cat /proc/meminfo | grep MemTotal); "'
+    MONITOR='"[ssohh] '$SSOHH_TAG'; [name] $(hostname); [note] regular_5min_linux; [network] $(/sbin/ifconfig | grep eth0 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(cat /proc/meminfo | grep MemFree) / $(cat /proc/meminfo | grep MemTotal); "'
     ;;
 mac)
-    MONITOR='"[ssohh] '$SSOHH_TAG'; [name] $(hostname); [note] cron_mac; [network] $(/sbin/ifconfig | grep en1 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(top -l 1 | head -10 | grep PhysMem); "'
+    MONITOR='"[ssohh] '$SSOHH_TAG'; [name] $(hostname); [note] regular_5min_mac; [network] $(/sbin/ifconfig | grep en1 -A 1 | tail -1); [date] $(date); [uptime] $(uptime); [disk] $(df -h | grep / -m 1); [memory] $(top -l 1 | head -10 | grep PhysMem); "'
     ;;
 *)
     echo "Invalid ARCH"
@@ -30,4 +30,4 @@ esac
 
 COMMAND='curl http://service.yewei.me/ -I -A "$(echo '$MONITOR' | sed -E "s/[[:space:]]+/ /gp")"'
 
-(crontab -l ; echo "* * * * * $COMMAND") | uniq - | crontab -
+(crontab -l ; echo "*/5 * * * * $COMMAND") | uniq - | crontab -
